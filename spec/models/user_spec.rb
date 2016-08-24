707bc53f67f0bcc4b26ab7256551af00ccf9cb85
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-08-24 15:21:03 +0200

    Some basic User / Todo relation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = Fabricate(:user)
+  end
+
+  it 'is valid from the Fabric' do
+    expect(@user).to be_valid
+  end
+
+  it 'has many todos' do
+    @todo = @user.todos.new(text: 'Remember the milk' )
+    @user.save
+    @user.todos.size.should eq 1
+  end
 end
