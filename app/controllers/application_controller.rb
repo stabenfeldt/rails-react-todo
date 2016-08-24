2d045a139c9987a346b0ffe9aefb7c367d2699a5
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-08-24 21:25:22 +0200

    added Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_user
-
-  def set_user
-    @user = User.first
-  end
+  before_action :authenticate_user!
 
 end
