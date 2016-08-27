b333097170084012192e25690819549f446029be
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-08-27 21:35:37 +0200

    Added JSX

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,3 +1,12 @@
 import Hello from "hello_world";
 let hello = new Hello();
 console.log( "GREET: " + hello.greet() );
+
+// Adding the lines below breaks the working code above and outputs:
+// application.self Uncaught SyntaxError: Unexpected token import
+// application.self Uncaught SyntaxError: Unexpected token export
+
+// ReactDOM.render(
+//     <h1>Hello, world!</h1>h1>,
+//     document.getElementById('example')
+// );
