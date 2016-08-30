d18b70ccc7513af8f3a09bd322ee932c89390470
Martin Stabenfeldt <martin@stabenfeldt.net>
2016-08-30 20:42:33 +0200

    React component listing colors

--- app/assets/javascripts/start.js.jsx
+++ app/assets/javascripts/start.js.jsx
@@ -1,5 +1,24 @@
 $(()=>{
-  ReactDOM.render(
-    <div>Hello martin!</div>, document.getElementById('example')
-  );
+
+
+var colors = ["Red","Green","Blue","Yellow","Black","White","Orange"];
+
+var List = React.createClass({
+  getInitialState: function() {
+    return {data: this.props.data};
+  },
+  render: function() {
+    return( <ul id="drag-n-drop">
+      {this.props.data.map(function(item) {
+        return <li key={item} >{item}</li>;
+      })}
+    </ul>)
+  }
+});
+
+var mountpoint = document.getElementById('example');
+if ( mountpoint ) {
+  ReactDOM.render( <List data={colors} />, document.getElementById('example'));
+}
+
 });
